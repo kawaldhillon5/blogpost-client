13a05cfdeedc6e064d9cde2cf8c669078ca31918
kawaldhillon5 <kawaldhillon5@gmail.com>
2025-01-23 00:35:26 -0500

    Update helper-functions.js

--- src/helper-functions.js
+++ src/helper-functions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.BACKEND_URL;
+const baseURL = 'http://fluorescent-silky-xenon.glitch.me';
 console.log(baseURL)
 axios.defaults.withCredentials = true;
 
