7b4c9ac1912742f07696163616d647dfab9755e3
kawaldhillon05 <kawaldhillon5@gmail.com>
2025-01-26 00:22:47 -0500

    updated css

--- src/helper-functions.js
+++ src/helper-functions.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL;
+const baseURL = 'http://localhost:3000/';
+// const baseURL = import.meta.env.VITE_API_URL;
 axios.defaults.withCredentials = true;
 
 export async function getAllBlogs(){
